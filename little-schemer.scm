5631d481ac798c9c3f1b84d4b2ddf5c62c14fffc
Phil Yeeles <phil@yeel.es>
2012-02-13 16:41:23 +0000

    page 53

--- little-schemer.scm
+++ little-schemer.scm
@@ -1,31 +1,63 @@
 #lang scheme
 (define atom?
-  (lambda (x)
+  (λ (x)
     (and (not (pair? x)) (not (null? x)))))
 (define lat?
-  (lambda (l)
+  (λ (l)
     (cond
       ((null? l) #t)
       ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
 (define member?
-  (lambda (a lat)
+  (λ (a lat)
     (cond
       ((null? lat) #f)
       (else (or (eq? (car lat) a)
                 (member? a (cdr lat)))))))
 (define rember
-  (lambda (a lat)
+  (λ (a lat)
     (cond
       ((null? lat) (quote ()))
       ((eq? (car lat) a) (cdr lat))
       (else (cons (car lat)
                   (rember a (cdr lat)))))))
 (define firsts
-  (lambda (l)
+  (λ (l)
     (cond
       ((null? l) (quote ()))
       ((null? (car l)) (firsts (cdr l)))
       (else (cons (car (car l))
                   (firsts (cdr l)))))))
-;; page 47
+
+(define insertR
+  (λ (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? old (car lat)) (cons old
+                                     (cons new
+                                           (cdr lat))))
+          (else (cons (car lat)
+                      (insertR new old (cdr lat)))))))
+
+(define insertL
+  (λ (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? old (car lat)) (cons new lat))
+          (else (cons (car lat)
+                      (insertL new old (cdr lat)))))))
+
+(define subst
+  (λ (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? old (car lat)) (cons new (cdr lat)))
+          (else (cons (car lat)
+                      (subst new old (cdr lat)))))))
+
+(define subst2
+  (λ (new o1 o2 lat)
+    (cond ((null? lat) (quote ()))
+          ((or (eq? o1 (car lat))
+               (eq? o2 (car lat))) (cons new (cdr lat)))
+          (else (cons (car lat)
+                      (subst2 new o1 o2 (cdr lat)))))))
+
+;; page 53
